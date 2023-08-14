02ef56a49c190b48121bf1fb2d52ab41307ab425
rimi <a.fatimaarimi@gmail.com>
2023-08-14 14:45:24 -0400

    imported navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { navLinks } from "../constants/index"
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -40,29 +40,29 @@ export const navLinks = [
 ];
 
 
-export const responsiveNavLinks = [
-  {
-    id: "my",
-    title: "My",
-  },
-  {
-    id: "skills",
-    title: "// skills",
-  },
-  {
-    id: "experience",
-    title: "// experience",
-  },
-  {
-    id: "projects",
-    title: "// projects",
-  },
-  {
-    id: "contact",
-    title: "// contact",
-  },
-  {
-    id: "resume",
-    title: "// resume",
-  },
-];
+// export const responsiveNavLinks = [
+//   {
+//     id: "my",
+//     title: "My",
+//   },
+//   {
+//     id: "skills",
+//     title: "// skills",
+//   },
+//   {
+//     id: "experience",
+//     title: "// experience",
+//   },
+//   {
+//     id: "projects",
+//     title: "// projects",
+//   },
+//   {
+//     id: "contact",
+//     title: "// contact",
+//   },
+//   {
+//     id: "resume",
+//     title: "// resume",
+//   },
+// ];
