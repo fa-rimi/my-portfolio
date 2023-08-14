20810a40bef89abc7f9896dcff89bd00b0aaafe2
rimi <a.fatimaarimi@gmail.com>
2023-08-14 03:43:15 -0400

    created folders and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
 import './App.css'
+import Homepage from './pages/Homepage'
 
 function App() {
 
   return (
     <div className='App h-screen w-screen'>
-    <h1>Hello</h1>
+      <Homepage/>
     </div>
   )
 }
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,7 @@
+export default function Homepage() {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+        </div>
+    )
+}
