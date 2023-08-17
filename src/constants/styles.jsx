e86d393c959f17ba16f0410046d7de3f5beef7a0
rimi <a.fatimaarimi@gmail.com>
2023-08-17 15:02:01 -0400

    .

--- src/constants/styles.jsx
+++ src/constants/styles.jsx
@@ -20,3 +20,5 @@ export const genSection = "w-screen py-[100px]";
 export const hoverFont = "font-['Cousine'] hover:font-[700] hover:italic px-4";
 export const title = "";
 export const subTitle = "text-[#222831] font-[Cousine] text-[14px] ";
+
+
