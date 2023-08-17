5f9be1edda39bf12ee34e4e58369b706a92fd4b4
rimi <a.fatimaarimi@gmail.com>
2023-08-17 15:12:17 -0400

    cleaned up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,13 @@
-// eslint-disable-next-line no-unused-vars
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { navLinks } from "../constants/index";
 import { hoverFont } from "../constants/styles";
 
 const Navbar = () => {
   const [isNavShrunk, setIsNavShrunk] = useState(false);
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [active, setActive] = useState("Home");
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-  
   const handleScroll = () => {
-    if (window.scrollY > 0) {
-      setIsNavShrunk(true);
-    } else {
-      setIsNavShrunk(false);
-    }
+    setIsNavShrunk(window.scrollY > 0);
   };
 
   useEffect(() => {
@@ -35,26 +24,22 @@ const Navbar = () => {
           isNavShrunk ? "shrink" : ""
         }`}>
         <ul className="w-screen py-10 px-20 sm:flex hidden justify-evenly items-center">
-          {/* We are mapping over our navlinks(getting one nav link, and index) */}
           {navLinks.map((nav) => (
             <li
               key={nav.id}
-              // active === nav.title ? "text-white" : "text-dimWhite"
-              // when link is active then the title will be white : otherwise dimwhite
               className={`${hoverFont} cursor-pointer text-[17px] ${
                 active === nav.title
                   ? "text-[#161B36] font-[700]"
                   : "text-black"
               }`}
               onClick={() => setActive(nav.title)}>
-              {/* within the href it is a # instead of / because # (fragment identifier) -- meaning in this single page application i am scrolling down to that section as oppose to / (path) which would take me to a different page all together (maybe later)*/}
               <a href={`#${nav.id}`}>{nav.title}</a>
             </li>
           ))}
         </ul>
 
         <div>
-          <img></img>
+          {/* <img alt="Logo"></img> */}
         </div>
       </nav>
     </div>
