2da461bf2d668b5de27bab2beed61f7707e3690d
rimi <a.fatimaarimi@gmail.com>
2023-08-19 20:09:13 -0400

    font styling when md

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
 import { ExternalLink } from "react-external-link";
 import { workExp } from "../constants/index";
